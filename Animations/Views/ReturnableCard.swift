88b5e27c54d7bd1bff69db4bfc50e45f825d8e12
saralune <sarahlef59@hotmail.fr>
2024-12-29 14:28:54 +0100

    Add 3 main views

---
+++ Animations/Views/ReturnableCard.swift
@@ -0,0 +1,18 @@
+//
+//  ReturnableCard.swift
+//  Animations
+//
+//  Created by Sara Lefort on 29/12/2024.
+//
+
+import SwiftUI
+
+struct ReturnableCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReturnableCard()
+}
