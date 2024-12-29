503d9f024d8c6be056f53da5dcc5b1e61e785387
saralune <sarahlef59@hotmail.fr>
2024-12-29 14:53:11 +0100

    Add cell

--- Animations/Views/RemovableCell.swift
+++ Animations/Views/RemovableCell.swift
@@ -8,11 +8,34 @@
 import SwiftUI
 
 struct RemovableCell: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+  let name: String = "Preview Name"
+      let date: String = "02/06/2014"
+      
+      var body: some View {
+          HStack {
+              VStack(alignment: .leading) {
+                  Text(name)
+                      .font(.title3)
+                      .bold()
+                  Text(date)
+                      .font(.callout)
+                      .fontWeight(.light)
+                      .foregroundStyle(.gray)
+              }
+          }
+          .frame(maxWidth: .infinity, alignment: .leading)
+          .padding()
+          .background(Color.white)
+          .clipShape(RoundedRectangle(cornerRadius: 16))
+          .shadow(color: .black.opacity(0.08), radius: 5, x: 0, y: 3)
+      }
+  
+  /// Help from Paul Hudscon : https://www.hackingwithswift.com/books/ios-swiftui/deleting-items-using-ondelete
+//  func removeRows(at offsets: IndexSet) {
+//    myTasks.remove(atOffsets: offsets)
+//  }
 }
 
 #Preview {
-    RemovableCell()
+  RemovableCell()
 }
