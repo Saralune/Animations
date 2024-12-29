aea0fc6d7a40318a26eaf657acdc8bb78e532b69
saralune <sarahlef59@hotmail.fr>
2024-12-29 15:27:23 +0100

    Add swipe to cell

--- Animations/Views/RemovableCell.swift
+++ Animations/Views/RemovableCell.swift
@@ -9,31 +9,49 @@ import SwiftUI
 
 struct RemovableCell: View {
   let name: String = "Preview Name"
-      let date: String = "02/06/2014"
-      
-      var body: some View {
-          HStack {
-              VStack(alignment: .leading) {
-                  Text(name)
-                      .font(.title3)
-                      .bold()
-                  Text(date)
-                      .font(.callout)
-                      .fontWeight(.light)
-                      .foregroundStyle(.gray)
-              }
-          }
-          .frame(maxWidth: .infinity, alignment: .leading)
-          .padding()
-          .background(Color.white)
-          .clipShape(RoundedRectangle(cornerRadius: 16))
-          .shadow(color: .black.opacity(0.08), radius: 5, x: 0, y: 3)
-      }
+  let date: String = "02/06/2014"
+  @State private var dragOffset = CGFloat.zero
   
-  /// Help from Paul Hudscon : https://www.hackingwithswift.com/books/ios-swiftui/deleting-items-using-ondelete
-//  func removeRows(at offsets: IndexSet) {
-//    myTasks.remove(atOffsets: offsets)
-//  }
+  
+  var body: some View {
+    ZStack {
+      //Background Rectnagle
+      Rectangle()
+        .frame(maxHeight: 77)
+        .clipShape(RoundedRectangle(cornerRadius: 16))
+        .foregroundStyle(Color.red)
+      
+      //Cell
+      VStack(alignment: .leading) {
+        Text(name)
+          .font(.title3)
+          .bold()
+        Text(date)
+          .font(.callout)
+          .fontWeight(.light)
+          .foregroundStyle(.gray)
+      }
+      .frame(maxWidth: .infinity, alignment: .leading)
+      .padding()
+      .background(Color.white)
+      .clipShape(RoundedRectangle(cornerRadius: 16))
+      .shadow(color: .black.opacity(0.08), radius: 5, x: 0, y: 3)
+      .offset(x: dragOffset)
+      .gesture(
+        DragGesture()
+          .onChanged { value in
+            // Balayage à gauche
+            if value.translation.width < 0 {
+              dragOffset = value.translation.width
+            }
+          }
+          .onEnded { _ in
+            dragOffset = .zero
+          }
+      )
+    }
+    .padding()
+  }
 }
 
 #Preview {
